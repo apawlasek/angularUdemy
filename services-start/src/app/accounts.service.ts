9ba7ebd4a4c16251d3ecadb3c44825ff98edae6a
Anna Pawlasek <anna.pawlasek@gmail.com>
2018-03-13 10:17:01 +0100

    logs

--- services-start/src/app/accounts.service.ts
+++ services-start/src/app/accounts.service.ts
@@ -19,20 +19,16 @@ export class AccountsService {
   ];
   statusUpdated = new EventEmitter<string>();
 
-  constructor(private loggingService: LoggingService) {}
+  constructor(private loggingService: LoggingService) {
+  }
 
   addAccount(name: string, status: string) {
     this.accounts.push({name: name, status: status});
-<<<<<<< HEAD
     this.loggingService.logStatusChange(status);
   }
+
   updateStatus(id: number, status: string) {
     this.accounts[id].status = status;
     this.loggingService.logStatusChange(status);
-=======
-  }
-  updateStatus(id: number, status: string) {
-    this.accounts[id].status = status;
->>>>>>> origin/master
   }
 }
