020e841be013a9a8b6539a39b239e72245082fc5
Anna Pawlasek <anna.pawlasek@gmail.com>
2018-03-13 10:11:51 +0100

    logs

--- services-start/src/app/accounts.service.ts
+++ services-start/src/app/accounts.service.ts
@@ -23,8 +23,10 @@ export class AccountsService {
 
   addAccount(name: string, status: string) {
     this.accounts.push({name: name, status: status});
+    this.loggingService.logStatusChange(status);
   }
   updateStatus(id: number, status: string) {
     this.accounts[id].status = status;
+    this.loggingService.logStatusChange(status);
   }
 }
